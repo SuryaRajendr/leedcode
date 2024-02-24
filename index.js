65e02adb943a0596ef33eebbb15044751aa92cb7
Surya Rajendran <73171944+SuryaRajendr@users.noreply.github.com>
2024-02-24 10:25:08 +0000

    two sum

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const arr = require("./arrays&hashing/groupanagran.js");
 const top_K_Element = require("./arrays&hashing/TopKFrequentElement.js");
 const product = require("./arrays&hashing/productofarrayExceptItself.js");
 const longestSequence = require("./arrays&hashing/longestConsecutiveSequence.js");
-const validPalindrome = require("./arrays&hashing/validPalindrome.js");
+const validPalindrome = require("./twoPointers/validPalindrome.js");
+
+const twoSum = require("./twoPointers/twoSum.js")
