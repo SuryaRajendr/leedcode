1a726c34d36bba7eb5653d4ccf99b13714c9786d
Surya Rajendran <73171944+SuryaRajendr@users.noreply.github.com>
2024-04-11 23:38:50 +0530

    removeNthFromEnd

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -48,37 +48,37 @@ function mergeSort(arr) {
 
 
 
-// function mergeSort(arr) {
-//     if (arr.length <= 1) {
-//         return arr;
-//     }
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
 
-//     const middle = Math.floor(arr.length / 2);
-//     const left = arr.slice(0, middle);
-//     const right = arr.slice(middle);
+    const middle = Math.floor(arr.length / 2);
+    const left = arr.slice(0, middle);
+    const right = arr.slice(middle);
 
-//     return merge(mergeSort(left), mergeSort(right));
-// }
+    return merge(mergeSort(left), mergeSort(right));
+}
 
-// function merge(left, right) {
-//     let result = [];
-//     let leftIndex = 0;
-//     let rightIndex = 0;
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
 
-//     while (leftIndex < left.length && rightIndex < right.length) {
-//         if (left[leftIndex] < right[rightIndex]) {
-//             result.push(left[leftIndex]);
-//             leftIndex++;
-//         } else {
-//             result.push(right[rightIndex]);
-//             rightIndex++;
-//         }
-//     }
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
 
-//     return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
-// }
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
 
-// // Example usage
-// const arr = [4, 2, 1, 3];
-// const sortedArr = mergeSort(arr);
-// console.log(sortedArr); // Output: [1, 2, 3, 4]
+// Example usage
+const arr = [4, 2, 1, 3];
+const sortedArr = mergeSort(arr);
+console.log(sortedArr); // Output: [1, 2, 3, 4]
