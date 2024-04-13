17bec00dd7e7062f1b57d60bc464278000f05821
Surya Rajendran <73171944+SuryaRajendr@users.noreply.github.com>
2024-04-13 21:10:29 +0530

    sortArrayByParity

---
+++ array/sortArrayByParity.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let odd = []
+    let even = []
+    for(let num of nums) {
+        if(num % 2 == 0) even.push(num)
+        else odd.push(num)
+    }
+    return even.concat(odd)
+    
+};
