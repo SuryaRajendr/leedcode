049b675e1f8226f92a488a7f67402a2f072e7a37
Surya Rajendran <73171944+SuryaRajendr@users.noreply.github.com>
2024-04-25 13:46:32 +0530

    mergeKSortedList

---
+++ linkedlist/mergeKSortedList.js
@@ -0,0 +1,25 @@
+const mergeKLists = function(lists) {
+    const queue = new MinPriorityQueue({ priority: x => x.val })
+  
+    for (const head of lists) {
+      if (head) {
+        queue.enqueue(head)
+      }
+    }
+  
+    let result = new ListNode()
+    const head = result
+  
+    while (!queue.isEmpty()) {
+      const { val, next } = queue.dequeue().element
+  
+      result.next = new ListNode(val)
+      result = result.next
+  
+      if (next) {
+        queue.enqueue(next)
+      }
+    }
+  
+    return head.next
+  }
